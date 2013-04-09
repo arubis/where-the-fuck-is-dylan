3f15c5bad46d4b4f41d78d0a23d3a14f8be83c52
Dylan Fitzgerald <dylan@dylanfitzgerald.net>
2013-04-09 21:46:05 +0000

    Add hooks for RedisCloud

--- wtfdylan.rb
+++ wtfdylan.rb
@@ -1,9 +1,19 @@
 # wtfdylan.rb
 require 'sinatra'
-require 'redis'
 
-redis = Redis.new
-redis.set("location", "AFRICA BITCHES")
+configure do
+  require 'redis'
+end
+
+configure :testing, :development do
+  REDIS = Redis.new
+  REDIS.set("location", "AFRICA BITCHES")
+end
+
+configure :production do
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 get '/' do
     haml :index
